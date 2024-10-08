8a1df7d313b22ce6c0c816df09d6e6d00071f561
Sixtiess <liamhhickey@icloud.com>
2024-10-08 12:58:37 -0700

    added fun factoid

--- webapp.py
+++ webapp.py
@@ -16,9 +16,11 @@ def home():
 def render_fact():
     states = get_state_options()
     state = request.args.get('state')
+    county1=population(state)
     county = county_most_under_18(state)
     fact = "In " + state + ", the county with the highest percentage of under 18 year olds is " + county + "."
-    return render_template('home.html', state_options=states, funFact=fact)
+    fact1 = "In " + state + ", the county with the highest people of under  year olds is " + county1 + "."
+    return render_template('home.html', state_options=states, funFact=fact, nikita=fact1)
     
 def get_state_options():
     """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
@@ -45,6 +47,19 @@ def county_most_under_18(state):
                 highest = c["Age"]["Percent Under 18 Years"]
                 county = c["County"]
     return county
+def population(state):
+    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
+    with open('demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    highest=0
+    county = ""
+    for c in counties:
+        if c["State"] == state:
+            if c["Population"]["2014 Population"] > highest:
+                highest = c["Population"]["2014 Population"]
+                county = c["County"]
+    return county
+
 
 def is_localhost():
     """ Determines if app is running on localhost or not
